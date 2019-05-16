596bb05616eaa6252cf354aed1c768dbc20b77c5
xwsoft <xiawensoft@126.com>
2019-05-16 19:05:43 +0800

    Test

---
+++ config-client/src/test/java/cn/updog/ConfigClientApplicationTests.java
@@ -0,0 +1,12 @@
+package cn.updog;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ConfigClientApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
