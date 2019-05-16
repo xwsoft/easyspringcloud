aa2879d5bc90046affa96562047d2e8088ad616f
xiawensoft@126.com <xwsoft1984>
2019-05-16 15:43:14 +0800

    Test

--- config-server/src/main/java/cn/updog/ConfigServerApplication.java
+++ config-server/src/main/java/cn/updog/ConfigServerApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class Springcloud7ConfigServerApplication {
+public class ConfigServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Springcloud7ConfigServerApplication.class, args);
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
 }
